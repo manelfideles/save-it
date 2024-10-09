1d6e89d0b3ecb679ea75d237797c3a8c702da5f0
Manuel Fideles <manuel.fideles@redlightsoft.com>
2024-10-09 17:19:48 +0100

    Removed poetry from app image

---
+++ scripts/build.sh
@@ -0,0 +1,6 @@
+# Optional uuid to tag the build
+UUID=$(uuidgen | tr A-F a-f)
+echo "Building save-it..."
+
+poetry export -f requirements.txt --output requirements.txt --without-hashes
+docker build -t save-it .
