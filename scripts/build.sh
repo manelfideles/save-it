78ca2771c4309f2a50e0affbb17e3cb3704ce851
Manuel Fideles <manuel.fideles@redlightsoft.com>
2024-10-17 11:19:26 +0100

    added platform arg to build command

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,9 @@
 # Optional uuid to tag the build
 UUID=$(uuidgen | tr A-F a-f)
+
 echo "Building save-it..."
 
 poetry export -f requirements.txt --output requirements.txt --without-hashes
-docker build -t manelfideles/save-it .
+docker build --platform linux/amd64 -t manelfideles/save-it .
+
+docker push manelfideles/save-it
